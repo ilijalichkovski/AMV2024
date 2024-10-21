ee968e50c42900ac78412ef8b116c4c2bd812f70
Ilija Lichkovski <62847974+ilijalichkovski@users.noreply.github.com>
2024-10-21 10:47:15 +0000

    updated intro

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Visualizing MRI scans of low-grade gliomas in Python")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Link format: [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+tab1, tab2, tab3, tab4 = st.tabs(["Introduction", 
+                                  "Materials & Methods", 
+                                  "Results", 
+                                  "Data availability"])
+tab1.header("Glioma")
+tab1.write("Lorem ipsum")
+tab1.header("Computed tomography (CT)")
+tab2.header("Choice of dataset")
+tab2.header("Patient choice")
+
+st.sidebar.write('hello')
