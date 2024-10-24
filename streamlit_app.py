6d02055b51c9f8b3df67261ef8830b6279960168
Ilija Lichkovski <62847974+ilijalichkovski@users.noreply.github.com>
2024-10-24 20:30:07 +0000

    some updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,4 +115,18 @@ with tab2:
 with tab3:
     st.header("Key visualizations")
 
+    with st.expander("See explanation"):
+        st.write('test')
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.header("A cat")
+            st.image("https://static.streamlit.io/examples/cat.jpg")
+
+        with col2:
+            st.header("A dog")
+            st.image("https://static.streamlit.io/examples/cat.jpg")
+
     st.header("Discussion")
+
+
